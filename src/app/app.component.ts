5ceeaad732343cf6b3f227a87e2373c238a20db7
Kgaogelo Thorn <kgaogelothorn@gmail,com>
2021-08-16 00:04:57 +0200

    refactor(app): updated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,35 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
-
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
   title = 'randomuser';
- userDetails: any = '';
- loading = true;
- userInfo = {value: '', type: '',};
- constructor(readonly http: HttpClient) {}
+  loading = true;
+  userDetails: any = '';
+  userInfo = { value: '', type: '' };
 
- ngOnInit() {
-   this.http.get('https://randomuser.me/api/')
-   .subscribe((response: any) => {
-     this.userDetails = response?.results[0];
-     this.userInfo = {
-       value: this.userDetails?.name?.first + ' ' + this.userDetails?.name?.last,
-       type: 'name',
-     }
-     this.loading = false
-   }, error => {
-     alert(error)
-   })
- }
+  constructor(readonly http: HttpClient) {}
 
- setValue(data: any) {
-    this.userInfo = {
-      value: data.value,
-      type: data.type,
-     }
-}
+  ngOnInit() {
+    this.http.get('https://randomuser.me/api/').subscribe(
+      (response: any) => {
+        this.userDetails = response?.results[0];
+        this.userInfo = {
+          value:
+            this.userDetails?.name?.first + ' ' + this.userDetails?.name?.last,
+          type: 'name',
+        };
+        this.loading = false;
+      },
+      (error) => {
+        alert(error);
+      }
+    );
+  }
+
+  setValue(data: any) {
+    this.userInfo = { value: data.value, type: data.type };
+  }
 }
