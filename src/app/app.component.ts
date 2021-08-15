de526f22f850b4c9f3325d25350888aeee4b3504
Kgaogelo Thorn <kgaogelothorn@gmail,com>
2021-08-15 23:57:20 +0200

    refactor(app): updated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-import { userData } from './user';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +7,31 @@ import { userData } from './user';
 })
 
 
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'randomuser';
- userDetails = userData;
+ userDetails: any = '';
+ loading = true;
+ userInfo = {value: '', type: '',};
+ constructor(readonly http: HttpClient) {}
 
+ ngOnInit() {
+   this.http.get('https://randomuser.me/api/')
+   .subscribe((response: any) => {
+     this.userDetails = response?.results[0];
+     this.userInfo = {
+       value: this.userDetails?.name?.first + ' ' + this.userDetails?.name?.last,
+       type: 'name',
+     }
+     this.loading = false
+   }, error => {
+     alert(error)
+   })
+ }
 
+ setValue(data: any) {
+    this.userInfo = {
+      value: data.value,
+      type: data.type,
+     }
 }
-
-
+}
