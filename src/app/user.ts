c33638f48de9d6c328641ea5ba56113b91a86e3d
Kgaogelo Thorn <kgaogelothorn@gmail,com>
2021-08-15 19:56:13 +0200

    refactor(randomuser) first commit:

---
+++ src/app/user.ts
@@ -0,0 +1,66 @@
+export const userData = {
+  "results": [
+      {
+          "gender": "female",
+          "name": {
+              "title": "Mrs",
+              "first": "Edeltrud",
+              "last": "Wolfrum"
+          },
+          "location": {
+              "street": {
+                  "number": 1020,
+                  "name": "Schulweg"
+              },
+              "city": "Teuschnitz",
+              "state": "Sachsen",
+              "country": "Germany",
+              "postcode": 39566,
+              "coordinates": {
+                  "latitude": "-39.9476",
+                  "longitude": "172.4484"
+              },
+              "timezone": {
+                  "offset": "-5:00",
+                  "description": "Eastern Time (US & Canada), Bogota, Lima"
+              }
+          },
+          "email": "edeltrud.wolfrum@example.com",
+          "login": {
+              "uuid": "3f841edc-5ead-4600-bc6c-b22f85243e5b",
+              "username": "silverpeacock224",
+              "password": "hellos",
+              "salt": "6Q3zdbEX",
+              "md5": "e5a4e7ce84b669756daea83c1ab8c543",
+              "sha1": "e6c1335fb24a73786fe8075a8f3fecc9b1713994",
+              "sha256": "f16cb81d032668d75f934cece5f79d6655bb1521b28910ff387a9c8d7b4797c5"
+          },
+          "dob": {
+              "date": "1952-03-16T09:14:57.296Z",
+              "age": 69
+          },
+          "registered": {
+              "date": "2018-10-19T18:55:32.297Z",
+              "age": 3
+          },
+          "phone": "0312-2937884",
+          "cell": "0179-9802418",
+          "id": {
+              "name": "",
+              "value": null
+          },
+          "picture": {
+              "large": "https://randomuser.me/api/portraits/women/54.jpg",
+              "medium": "https://randomuser.me/api/portraits/med/women/54.jpg",
+              "thumbnail": "https://randomuser.me/api/portraits/thumb/women/54.jpg"
+          },
+          "nat": "DE"
+      }
+  ],
+  "info": {
+      "seed": "b226f449610868f6",
+      "results": 1,
+      "page": 1,
+      "version": "1.3"
+  }
+}
